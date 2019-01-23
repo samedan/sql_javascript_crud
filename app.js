dbf463d2c4ff7e2cf73a2ad0bda7bc7a19d890be
samedan <same.dan@gmail.com>
2019-01-23 17:37:04 +0100

    End of POST

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Database
+const db = require('./config/database');
+
+// Test DB
+db.authenticate()
+  .then(() => console.log('Database connected'))
+  .catch(err => console.log('Error' + err));
+
+const app = express();
+
+// Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// BodyPOarser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.render('index', { layout: 'landing' }));
+
+// Gig Routes
+app.use('/gigs', require('./routes/gigs'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
